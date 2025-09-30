1cb51740ba5ca6ba5e2f428e9b26cf67122ab4d7
Manuel <jomangonzalez321@gmail.com>
2025-09-30 18:09:14 +0800

    s

--- Struct/build.sh
+++ Struct/build.sh
@@ -1,7 +1,20 @@
+# set -o errexit
+
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --no-input
+
+# python manage.py migrate
+
+#!/bin/bash
 set -o errexit
 
+# Move into backend folder
+cd Struct/backend
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Django setup
 python manage.py collectstatic --no-input
-
 python manage.py migrate
