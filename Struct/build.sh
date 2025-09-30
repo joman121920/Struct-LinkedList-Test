ba0ecb66fa81454d5a53081ad0a8f76e304588c1
Manuel <jomangonzalez321@gmail.com>
2025-09-30 19:27:58 +0800

    updated to get struct

--- Struct/build.sh
+++ Struct/build.sh
@@ -2,14 +2,14 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python struct/manage.py collectstatic --no-input
 
-python manage.py migrate
+python struct/manage.py migrate
 
 
 if [[ $CREATE_SUPERUSER ]]
 then
-    python manage.py createsuperuser --no-input
+    python struct/manage.py createsuperuser --no-input
 fi
 
 # #!/bin/bash
