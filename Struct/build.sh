f0544929d1a8b0d5319bb0add10de8f1a6f8bdbb
Manuel <jomangonzalez321@gmail.com>
2025-09-30 19:23:53 +0800

    create superuser

--- Struct/build.sh
+++ Struct/build.sh
@@ -1,19 +1,25 @@
-# set -o errexit
-
-# pip install -r requirements.txt
-
-# python manage.py collectstatic --no-input
-
-# python manage.py migrate
-
-#!/bin/bash
 set -o errexit
-# Install dependencies
+
 pip install -r requirements.txt
 
-# Move into backend folder
-cd Struct 
-
-# Django setup
 python manage.py collectstatic --no-input
+
 python manage.py migrate
+
+
+if [[ $CREATE_SUPERUSER ]]
+then
+    python manage.py createsuperuser --no-input
+fi
+
+# #!/bin/bash
+# set -o errexit
+# # Install dependencies
+# pip install -r requirements.txt
+
+# # Move into backend folder
+# cd Struct 
+
+# # Django setup
+# python manage.py collectstatic --no-input
+# python manage.py migrate
