03c9fe03eb0c57a5611094b6224fd92ec31344a9
Manuel <jomangonzalez321@gmail.com>
2025-09-30 19:26:03 +0800

    static root added

--- Struct/backend/deployment_settings.py
+++ Struct/backend/deployment_settings.py
@@ -2,6 +2,12 @@ import os
 import dj_database_url
 from .settings import * 
 from .settings import BASE_DIR
+import os
+
+STATIC_URL = '/static/'
+
+# Add this:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
