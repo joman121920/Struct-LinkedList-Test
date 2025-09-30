1e19d6558bc2b251ca6b3e49866223c1ea4d09ac
Manuel <jomangonzalez321@gmail.com>
2025-09-30 19:52:29 +0800

    from chatgpt

--- Struct/build.sh
+++ Struct/build.sh
@@ -1,24 +1,23 @@
+@'
 #!/bin/bash
 set -o errexit
-
-# Install dependencies
+python -m pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 
-# Go to folder where manage.py is
 cd Struct
 
 
 
-# Django setup
 python manage.py collectstatic --no-input
 python manage.py migrate
+'@ | Set-Content -Path Struct\build.sh -Encoding UTF8
 
 
 
-if [[ $CREATE_SUPERUSER ]]
-then
-    python struct/manage.py createsuperuser --no-input
-fi
+# if [[ $CREATE_SUPERUSER ]]
+# then
+#     python struct/manage.py createsuperuser --no-input
+# fi
 
 # #!/bin/bash
 # set -o errexit
