0d97b5338c42247eb2e2723d1638be2d1cf1b8cb
Manuel <jomangonzalez321@gmail.com>
2025-09-30 21:42:23 +0800

    build.shj fix

--- Struct/build.sh
+++ Struct/build.sh
@@ -11,23 +11,3 @@ cd Struct
 # # Django setup2
 python manage.py migrate
 
-
-
-
-
-# if [[ $CREATE_SUPERUSER ]]
-# then
-#     python struct/manage.py createsuperuser --no-input
-# fi
-
-# #!/bin/bash
-# set -o errexit
-# # Install dependencies
-# pip install -r requirements.txt
-
-# # Move into backend folder
-# cd Struct 
-
-# # Django setup
-# python manage.py collectstatic --no-input
-# python manage.py migrate
